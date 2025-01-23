9f28b9bca977858fc1af3193be5309e0b2fbceb0
Ashrat <ashrat.ali@nimapinfotech.com>
2025-01-23 18:44:07 +0530

    changes done

--- commonarray.js
+++ commonarray.js
@@ -3,12 +3,12 @@ function commonarray(arr,arr1){
     for(let i = 0;i< arr.length;i++){
         for(let j = 0; j < arr1.length - 1;j++){
             if(arr[i] === arr1[j] && arr[i + 1] === arr1[j + 1]){
-                int.push(arr[i],arr[i + 1]);
+                intersecarray.push([arr[i],arr[i + 1]]);
             }
         }
     }
     return intersecarray;
 }
-const arr = [1,6,4,3,9,5];
-const arr1 = [8,9,2,8,1,4,3];
+const arr = [1,3,3,6,4,3,9,5];
+const arr1 = [8,9,2,8,1,3,4,3];
 console.log(commonarray(arr,arr1))
