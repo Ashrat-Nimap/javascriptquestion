79e30538f427e90493a25c790caffd3dac157a7b
Ashrat <ashrat.ali@nimapinfotech.com>
2025-02-04 18:08:43 +0530

    solving problem

--- commonarray.js
+++ commonarray.js
@@ -1,13 +1,13 @@
 function commonarray(arr,arr1){
-    let intersecarray = [];
-    for(let i = 0;i< arr.length;i++){
-        for(let j = 0; j < arr1.length - 1;j++){
-            if(arr[i] === arr1[j] && arr[i + 1] === arr1[j + 1]){
-                intersecarray.push([arr[i],arr[i + 1]]);
+        let intersecarray = [];
+        for(let i = 0;i< arr.length;i++){
+            for(let j = 0; j < arr1.length - 1;j++){
+                if(arr[i] === arr1[j] && arr[i + 1] === arr1[j + 1]){
+                    intersecarray.push([arr[i],arr[i + 1]]);
+                }
             }
         }
-    }
-    return intersecarray;
+        return intersecarray;
 }
 const arr = [1,3,3,6,4,3,9,5];
 const arr1 = [8,9,2,8,1,3,4,3];
--- duplicatevalueinarray.js
+++ duplicatevalueinarray.js
@@ -6,16 +6,11 @@ function duplicate(arr) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] === arr[j]) {
                 arr1.push(arr[i]);
-            }
-            // if (arr1.includes(arr[j])) {
-            //     num=false;
-            //     break;
-            // }
-        }
+            }  
+      }
 
     }
     return arr1;
 }
 const arr = [1, 2, 3, 2, 5, 7,2]
 console.log(duplicate(arr));
-
